6608ed4b5a680612a701092727c2cde8fc1076ad
Ahmed Radwan <aradwann@proton.me>
2023-03-01 18:37:34 +0200

    initial commit

---
+++ Cart.js
@@ -0,0 +1,18 @@
+export class Cart {
+    constructor() {
+        this.items = [];
+    }
+
+    addToCart(item) {
+        this.items.push(item);
+    }
+
+    removeFromCart(item) {
+        for (let i = 0; i < this.items.length; i++) {
+            const currentItem = this.items[i];
+            if (currentItem === item) {
+                this.items.splice(i, 1);
+            }
+        }
+    }
+}
